9ded6d62dd8a3a05184de9ee069a6233fd30f773
calixtojj <calixtosjefferson@gmail.com>
2019-09-03 15:43:18 +0200

    Data Attributes  Nome created

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,12 @@
 class Pessoa:
+    def __init__(self, nome=None):
+        self.nome = nome
     def cumprimentar(self):
         return 'Olá'
 
 if __name__ == "__main__":
-    p = Pessoa()
+    p = Pessoa('Maria')
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'José'
+    print(p.nome)
